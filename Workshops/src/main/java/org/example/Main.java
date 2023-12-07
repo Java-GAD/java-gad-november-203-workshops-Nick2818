b74b10ddae86e5a02f66a0c76147cc1cb1b4c192
Nicky M28 <musuroi.niky@gmail.com>
2023-12-07 21:33:40 +0200

    first loop problems solved

--- Workshops/src/main/java/org/example/Main.java
+++ Workshops/src/main/java/org/example/Main.java
@@ -1,7 +1,61 @@
 package org.example;
 
 public class Main {
+
+    // 1
+    public static void printHallowPattern(int n){
+
+        for(int i = 0; i < n; i++){
+
+            for(int j = 0; j < n; j++){
+
+                if( i==0 || i == n-1 || j == 0 || j == n-1 ){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    // 2
+
+    public static void printLefttHalfTriangle(int n){
+
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(j <= i){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printLefttHalfTriangle2(int n){
+
+        for(int i = 0 ; i < n; i++){
+
+            printCustomLine(i+1);
+            System.out.println();
+        }
+    }
+
+    public static void printCustomLine(int n){
+        for(int i = 0; i < n; i++){
+            System.out.print("*");
+        }
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+       // printHallowPattern(6);
+        printLefttHalfTriangle(6);
+        System.out.println();
+        printLefttHalfTriangle2(6);
     }
 }
